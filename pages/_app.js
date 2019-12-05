f45517917a963f63c2f99f762d0e3a386226a41f
allpwrfulroot <akunz@alum.mit.edu>
2019-12-04 23:16:21 -0500

    reset _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,30 +3,15 @@ import App from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'utils/global-style'
 import theme from 'utils/theme'
-import { Layout } from 'components'
-
-import 'isomorphic-unfetch'
 
 export default class MyApp extends App {
-  state = {
-    count: 0,
-  }
-
-  upCount = () => this.setState({ count: this.state.count + 1 })
-
   render() {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
         <>
           <GlobalStyle />
-          <Layout>
-            <Component
-              {...pageProps}
-              count={this.state.count}
-              upCount={this.upCount}
-            />
-          </Layout>
+          <Component {...pageProps} />
         </>
       </ThemeProvider>
     )
